418a48bfb280507178e2f338a3bb4dde4001cb61
LeoCR <leonardoas2007@gmail.com>
2021-02-20 15:20:02 -0600

    Updating Route for logging out

--- src/containers/App.js
+++ src/containers/App.js
@@ -170,6 +170,45 @@ class App extends React.Component {
                     </div> 
                 </React.Fragment>
             }/>
+            <Route path='/logout' exact render={()=>
+                <React.Fragment>
+                    <h1>Menu</h1>
+                    <div className="row">
+                      <div className="col-xs-5 col-md-4" > 
+                        <Link to="/appetizers" className="anchor_menu">
+                              <h4>Appetizers</h4>
+                              <figure>
+                                  <img src="images/icon_entrees.png" className="icon_service" alt="Appetizers"/>
+                              </figure>
+                        </Link>
+                      </div>
+                      <div className="col-xs-5 col-md-4">
+                        <Link to="/main-courses" className="anchor_menu">
+                              <h4>Main Courses</h4>
+                              <figure>
+                                <img src="images/icon_lunch.jpg" className="icon_service" alt="Strong Dishes"/>
+                              </figure>
+                        </Link>
+                      </div>
+                      <div className="col-xs-5 col-md-4">
+                        <Link to="/desserts" className="anchor_menu">
+                              <h4>Desserts</h4>
+                              <figure>
+                                  <img src="images/icon_dessert.png" className="icon_service" alt="Desserts"/>
+                              </figure>
+                        </Link>
+                      </div>
+                      <div className="col-xs-5 col-md-4 last-col">
+                          <Link to="/drinks" className="anchor_menu">
+                              <h4>Drinks</h4>
+                              <figure>
+                                  <img src="images/icon_drinks.jpg" className="icon_service" alt="Drinks"/>
+                              </figure>
+                        </Link>  
+                      </div>
+                    </div> 
+                </React.Fragment>
+            }/>
             <Route path="/desserts" exact render={()=><React.Fragment>
                 <Desserts Desserts={this.props.desserts} title="Desserts" 
                   addProductToCart={this.addProductToCart} 
